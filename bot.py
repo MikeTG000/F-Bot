22c2d2d4ed94268643ab28d73e826d80940a79a6
MikeTG000 <119830644+MikeTG000@users.noreply.github.com>
2023-06-20 21:37:05 +0530

    Update and rename zxmx.py to bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import tgcrypto
 from pyrogram import Client, filters
 
 Bot = Client(
-    "YouTube-Thumbnail-Downloader",
+    "FirstBot",
     bot_token = os.environ.get("BOT_TOKEN"),
     api_id = int(os.environ.get("API_ID")),
     api_hash = os.environ.get("API_HASH")
